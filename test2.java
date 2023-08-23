84af31f2c211e31a0136aa8c077f21e311d11108
kimgyoung <rlarkdud277@gmail.com>
2023-08-23 17:51:56 +0900

    test

--- test2.java
+++ test2.java
@@ -92,7 +92,7 @@ public class test2 {
         */
 
         // 선언과 동시에 초기화
-        int number = 10;
+        //int number = 10;
 
 
         // for (반복문)
@@ -101,11 +101,14 @@ public class test2 {
         // for(초기문; 조건문; 증감문)
 
 
+       /*
         for (int i = 0; i < 10; ++i)
         {
             System.out.println(i + "Hello World");
 
         }
+
+        */
 
 
         // while (반복문)
@@ -114,6 +117,7 @@ public class test2 {
         // if (횟수를 정해 놓고 쓸 때는) 무조건 초기문, 조건문, 증감문 필요
 
         // 초기문
+        /*
         int i= 10;
 
         // 조건문
@@ -122,6 +126,134 @@ public class test2 {
             // 증감문
             ++i;
         }
+
+         */
+
+        // 구구단 출력 (2~9단) , for문 중복사용
+
+
+
+
+        /*
+        for (int d = 2; d <= 9; ++d)
+        {
+            for (int e =)
+                System.out.println("")
+        }
+
+         */
+
+
+
+        /*
+        for (int i = 2; i <= 9; ++i )
+        {
+            for (int j = 1 ; j <= 9 ; ++j)
+                System.out.println(i + "*" + j + "=" + i*j);
+
+        }
+         */
+        // 짝수 : 출력에서 *2 하기 (j*2) i*j
+
+
+
+        /*
+        for (int i = 2; i <= 9; ++i )
+        {
+            for (int j = 1 ; j <= 9 ; ++j)
+
+
+
+                System.out.println(i + "*" + j + "=" + i*j );
+
+        }
+
+         */
+
+        // 구구단
+        // @ 예외 처리: 짝수 출력할 때 if문 (i%2 == 0)은 홀수 출력을 제외 하는거라  좀 비효율
+
+        /*
+        for(int i = 2; i < 10; ++i)
+        {
+            for(int j = 1; j < 10; ++j)
+            {
+                System.out.println(i + "x" + j + "=" + (i*j));
+            }
+        }
+        */
+
+        // 짝수만 출력
+        /*
+
+        for(int i = 2; i < 10; i += 2)
+        {
+            for(int j = 1; j < 10; ++j)
+            {
+                System.out.println(i + "x" + j + "=" + (i*j));
+            }
+
+        }
+
+         */
+
+        // 1626년도 미국 인디언이 맨헤튼 섬 24$에 판매
+        // 저금 매년 3% 금리? = 현재 - 복리이자
+        // while
+
+        /*
+        while(int i = 1626, i < 2024; i++)
+        {
+            while(int j = 24, j< ; j++)
+            {
+                System.out.println( i + "년" + j*(1.03) + "=" + "달러");
+            }
+
+        }
+
+         */
+
+        /*
+        int year = 2023 - 1626;
+        double money = 24.0;
+        int i = 0;
+
+        // float money = 24.0f;
+
+
+        while(i < year)
+        {
+            ++i;
+            money += money * 0.03;
+        }
+        System.out.println(money);
+
+         */
+
+        int year = 1626;
+        double money = 24.0;
+
+        while(year < 2023)
+        {
+            year++;
+            money *= 1.03;
+        }
+        System.out.println(money);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
